697dc93c32e7b932562f05b4bf019d9d3d8e61e8
Ankit Sharma <ankittiger7256@gmail.com>
2026-07-27 02:29:02 +0530

    fix my code

--- password.py
+++ password.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import random
 
 len = int(input("enter the password length"))
@@ -10,17 +10,4 @@ password = ""
 for i in range(len):
     password += random.choice(characters) 
 
-=======
-import random
-
-len = int(input("enter the password length"))
-
-characters =  "1234567890ABCDEFGHIJKLabcdefghijkl@#$*&"
-
-password = ""
-
-for i in range(len):
-    password += random.choice(characters) 
-
->>>>>>> 735208c1b1cc1c600577736dadadd8aaecb8bc03
 print("Your Password is = ", password)
